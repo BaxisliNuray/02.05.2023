4334074afcc44c8ea3cbed24407f98de592b05e8
BaxisliNuray <bakhishlinuray@gmail.com>
2023-05-02 02:02:35 +0400

    first-react-app

---
+++ first-react-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import About from './pages/About';
+
+
+
+function App() {
+  return (
+    <React.Fragment>
+
+     <About/>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ first-react-app/src/pages/About/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navbar from '../../components/Navbar'
+import Contact from '../Contact'
+import Slider from '../Slider'
+import Footer from '../../components/Footer'
+
+const About = () => {
+  return (
+    <>
+      <Navbar />
+      <div>About Page</div>
+      <Contact />
+      <Slider/>
+      <Footer/>
+    </>
+  )
+}
+
+export default About
